9fa0da3fba7d8336a264000987aa179b28283798
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-21 18:42:41 +0000

    add page

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ delete_page = st.Page('delete_py', title='Delete Page')
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title='Data manager')
 pg.run()
+
+st.Page('pages/lab1.py', title='Lab 1', icon=None, url_path=None, default=False)
+
