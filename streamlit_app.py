41ef65491b42d1ffb13370afb6a09cff36edff51
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-21 13:29:32 -0500

    Initial commit page creation

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+create_page = st.Page('create_py', title='Create Page')
+delete_page = st.Page('delete_py', title='Delete Page')
+
+pg = st.navigation([create_page, delete_page])
+st.set_page_config(page_title='Data manager')
+pg.run()
