028d192ce948cb1b01d4300849919f0868f7e948
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-21 18:44:34 +0000

    deleted create/delete

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,5 @@ import streamlit as st
 st.markdown('# 488 Labs')
 st.markdown('## :red[Ava Langanki]')
 
-create_page = st.Page('create_py', title='Create Page')
-delete_page = st.Page('delete_py', title='Delete Page')
-
-pg = st.navigation([create_page, delete_page])
-st.set_page_config(page_title='Data manager')
-pg.run()
-
 st.Page('pages/lab1.py', title='Lab 1', icon=None, url_path=None, default=False)
 
