c2f9dfaf90209bf0734e991d0940ab69149d9dad
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-21 18:47:39 +0000

    lab1 path

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.markdown('# 488 Labs')
 st.markdown('## :red[Ava Langanki]')
 
-st.Page('pages/lab1.py', title='Lab 1', icon=None, url_path=None, default=False)
+st.Page('/workspaces/document-qa/lab1.py', title='Lab 1', icon=None, url_path=None, default=False)
 
