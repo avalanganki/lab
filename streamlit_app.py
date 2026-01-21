67457df6e05bb547613c8ce8e68859ff6b3e3878
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-21 18:49:49 +0000

    navigation intro #

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.markdown('# 488 Labs')
 st.markdown('## :red[Ava Langanki]')
 
-st.Page('/workspaces/document-qa/lab1.py', title='Lab 1', icon=None, url_path=None, default=False)
+p1 = st.Page('/workspaces/document-qa/lab1.py', title='Lab 1', icon=None, url_path=None, default=False)
 
+pg = st.navigation([p1])
