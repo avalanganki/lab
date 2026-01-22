2137a6fcdb6b0af1bea114847603704d37ea2d72
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-22 01:00:27 +0000

    path

--- pageconfig.py
+++ pageconfig.py
@@ -12,7 +12,7 @@ st.markdown('# 488 Labs')
 st.markdown('## :red[Ava Langanki]')
 
 # Create pages - reference the files directly
-p1 = st.Page('lab1.py', title='Lab 1 - Document QA', icon='📄', default=False)
+p1 = st.Page('/workspaces/lab/Labs/lab1.py', title='Lab 1 - Document QA', icon='📄', default=False)
 p2 = st.Page('lab2.py', title='Lab 2 - Document Summarizer', icon='📝', default=True)
 
 # Create navigation
