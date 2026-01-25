bf01797740766bc5aebb7a3a7514b716fa1fd09f
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-25 18:34:28 +0000

    idk

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.markdown('# 488 Labs')
 st.markdown('## :red[Ava Langanki]')
 
 p1 = st.Page('/workspaces/lab/Labs/lab1.py', title='Lab 1 - Document QA', icon='📄', default=False)
-p2 = st.Page('lab2.py', title='Lab 2 - Document Summarizer', icon='📝', default=True)
+p2 = st.Page('/workspaces/lab/Labs/lab2.py', title='Lab 2 - Document Summarizer', icon='📝', default=True)
 
 pg = st.navigation([p1, p2])
 
