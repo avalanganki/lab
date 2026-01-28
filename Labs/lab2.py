e275806724b924f157eb8ac0aac75126de0356e9
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-28 17:44:57 +0000

    remove text

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -2,10 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 st.title("My app using secrets")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
+
 
 secret_key = st.secrets.OPENAI_API_KEY
 client = OpenAI(api_key=secret_key) 
