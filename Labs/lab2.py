350a05fe585dd7bfbe73cef23380ac5038fb9851
avalanganki <152558672+avalanganki@users.noreply.github.com>
2026-01-28 18:43:15 +0000

    part c changes

--- Labs/lab2.py
+++ Labs/lab2.py
@@ -2,28 +2,34 @@ import streamlit as st
 from openai import OpenAI
 
 st.title("Document QA With Secret")
-
-
 secret_key = st.secrets.OPENAI_API_KEY
 client = OpenAI(api_key=secret_key) 
 
+st.sidebar.title("Options")
+options = st.sidebar.selectbox(
+    'How would you like it to be answered?',
+    ('100 Words Basic', 'Two Paragraphs', '5 Bullet Points')
+)
+if options == '100 Words Basic':
+    prompt_instruction = "Summarize this document in exactly 100 words"
+elif options == 'Two Paragraphs':
+    prompt_instruction = "Summarize this document in two connecting paragraphs"
+elif options == '5 Bullet Points':
+    prompt_instruction = "Summarize this document in five bullet points"
+
+model_list = ['gpt-4o-mini', 'gpt-4o-nano']
+selected_model = st.sidebar.selectbox('Select Model', model_list)
 uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
 )
 
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
-
-if uploaded_file and question:
+if uploaded_file and options:
 
     document = uploaded_file.read().decode()
     messages = [
         {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"{prompt_instruction}\n\nDocument: {document}",
         }
     ]
 
@@ -35,4 +41,12 @@ if uploaded_file and question:
     )
 
     st.write_stream(stream)
+
+    #lab 2 p.C instructions
+    # provide user with options 
+    # summarize in 100 words
+    # summarize in two connecting paragraphs
+    # summarize in 5 bullet 
+    # let user select btwn models (mini / nano)
+    # have a checkbox for 'advanced model'
     
